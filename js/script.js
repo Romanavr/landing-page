aff246583588261af0a68d095f0594c41db64bc2
Roman Avr <rosin2134@gmail.com>
2017-04-14 22:10:03 +0300

    Исправлено отображение контакой формы Поправлена отображение между секциями(больше нет отступов) Исправлена отображение картинок в блоке "Сервисы", поправлена верстка flex'ов Блок "Получите выписку ЕГРН" также получил пере-верстку флексов, теперь нормально центрируется Убрана старая карта, добавлена карта с собственной иконкой от дизайна Артемия Лебедва, в иконке отображена доп.инфа Футер вертикально отцентрирован(наконец-то)

---
+++ js/script.js
@@ -0,0 +1,26 @@
+YMaps.jQuery(function () {
+        // Создает экземпляр карты и привязывает его к созданному контейнеру
+        var map = new YMaps.Map(YMaps.jQuery("#YMapsID")[0]);
+
+        // Устанавливает начальные параметры отображения карты: центр карты и коэффициент масштабирования
+        map.setCenter(new YMaps.GeoPoint(37.632726, 55.758809), 15);
+
+        // Создаем очень уникальную-метку по дизайну Артема Лебдева
+        var s = new YMaps.Style();
+        s.iconStyle = new YMaps.IconStyle();
+
+        // Делаем её размеры и пропорции по феншую
+        s.iconStyle.href = 'images/map-label.png';
+        s.iconStyle.size = new YMaps.Point(60, 80);
+        s.iconStyle.offset = new YMaps.Point(-20, -70);
+
+        // Создает уникальную метку в центре Москвы
+        var placemark = new YMaps.Placemark(new YMaps.GeoPoint(37.632706,55.758658), {style: s});
+
+        // Устанавливаем уникальное содержимое балуна
+        placemark.name = "Москва";
+        placemark.description = "Никольная дом 10 стр.2, офис 315. <p><a class='map-tel-link' href='tel: +7-495-111-11-06'><i style='color: #d62026' class='fa fa-phone'></i><strong>+7 (495) 111-11-06</strong></a></p>";
+
+        // Добавляет метку на карту
+        map.addOverlay(placemark);
+});
