946f5d63251304ce24d581cf65871cba58eeeff3
Alexey Toklovich <alexey@toklovich.com>
2017-05-03 18:43:01 +0700

    Design changes and fixes.

--- js/script.js
+++ js/script.js
@@ -14,16 +14,16 @@ $(document).ready(function () {
         var $form = $(this);
         var formData = new FormData($form[0]);
 
-        $("#myModal").show();
-        $("#myModal input[name='service-name']").val(formData.get("service-name"));
-        $("#myModal input[name='service-price']").val(formData.get("service-price"));
-        $("#myModal input[name='stamp']").val(formData.get("stamp"));
+        $(".js-overlay").addClass('active');
+        $("#feedback-modal input[name='service-name']").val( $form.find('input[name="service-name"]').val() );
+        $("#feedback-modal input[name='service-price']").val( $form.find('input[name="service-price"]').val() );
+        $("#feedback-modal input[name='stamp']").val( $form.find('input[name="stamp"]').val() );
 
         return false;
     });
 
     $(".modal-close").click(function () {
-        $("#myModal").hide();
+        $(".js-overlay").removeClass('active');
     });
 
     $("[data-type]").click(function () {
